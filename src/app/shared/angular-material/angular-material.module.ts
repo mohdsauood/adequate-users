95f043100b71df49f278af707277a6c600dba0d6
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2022-01-29 14:58:25 +0400

    feat : add angular material module

---
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -0,0 +1,40 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+/**
+ * all necessary material module imports for shared module
+ */
+const materialModule: any = [
+  LayoutModule,
+  MatGridListModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonToggleModule,
+  MatMenuModule,
+  MatRippleModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatDialogModule,
+];
+
+@NgModule({
+  imports: [...materialModule],
+  exports: [...materialModule],
+})
+export class AngularMaterialModule {}
