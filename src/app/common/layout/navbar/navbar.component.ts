696804c7244f0c3304a39cf343815d6dffbadab0
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2022-01-30 11:50:09 +0400

    feat : add dynamic nav button

--- src/app/common/layout/navbar/navbar.component.ts
+++ src/app/common/layout/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isLoginPage = false;
+  constructor(public router: Router) {}
 
   ngOnInit(): void {
+    this.router.events
+      .pipe(filter((r) => r instanceof NavigationEnd))
+      .subscribe((route) => {
+        this.isLoginPage = (route as NavigationEnd).url.includes('login');
+      });
   }
-
 }
